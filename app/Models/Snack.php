6d75e3e19566c0c69b449fa5b8c8be7bb8a8c418
shotanuno <20ca049e@rikkyo.ac.jp>
2022-11-18 20:55:58 +0900

    add: Modelの編集 Snackモデルにfillableを追加した.

--- app/Models/Snack.php
+++ app/Models/Snack.php
@@ -10,6 +10,11 @@ class Snack extends Model
 {
     use HasFactory;
     
+    protected $fillable =[
+        'name',
+        'overview'
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
