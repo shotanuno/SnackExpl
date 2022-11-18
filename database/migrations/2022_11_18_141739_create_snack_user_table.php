d0e5c3f532591b56694785710c5b15f5d6344368
shotanuno <20ca049e@rikkyo.ac.jp>
2022-11-18 14:20:18 +0900

    add: migrationファイルの作成 snack_userテーブルのマイグレーションファイルを作成した

---
+++ database/migrations/2022_11_18_141739_create_snack_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('snack_user', function (Blueprint $table) {
+            $table->foreignId('snack_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->primary(['snack_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('snack_user');
+    }
+};
