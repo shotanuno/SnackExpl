7f45eb9227e6d7efe3325506b7a04f3875b71129
shotanuno <20ca049e@rikkyo.ac.jp>
2022-11-29 20:21:05 +0900

    add: CommentPolicyの登録をした.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         User::class => UserPolicy::class,
+        Comment::class => CommentPolicy::class,
     ];
 
     /**
